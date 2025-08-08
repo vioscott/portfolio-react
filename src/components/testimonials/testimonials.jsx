86d658269e6641021409518518746204575d274b
vioscott <vioscott2020@gmail.com>
2025-08-08 07:00:34 -0700

    Fix CSS import case sensitivity issues for Netlify deployment

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import "./Testimonials.css";
+import "./testimonials.css";
 import { addTestimonial, getApprovedTestimonials } from "../../firebase/testimonialService";
 
 const containerVariants = {
